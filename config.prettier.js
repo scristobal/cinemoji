0c3c835bedbaf83257bb770fbf17bc065116c4a7
Samuel Cristobal <el.tobal@gmail.com>
2023-04-08 22:01:25 +0200

    added prettier tailwind plugin

---
+++ config.prettier.js
@@ -0,0 +1,10 @@
+module.exports = {
+  useTabs: false,
+  printWidth: 120,
+  tabWidth: 4,
+  singleQuote: true,
+  trailingComma: "none",
+  bracketSameLine: true,
+  semi: true,
+  plugins: [require("prettier-plugin-tailwindcss")],
+};
